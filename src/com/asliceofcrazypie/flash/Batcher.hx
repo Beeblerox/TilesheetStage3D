6aed3273dac4e5c4a24e0d3cc8c46eb6ea66de9e
Zaphod <zaphy@yandex.ru>
2015-05-03 14:34:49 +0300

    starting converting some flixel code into batcher

--- src/com/asliceofcrazypie/flash/Batcher.hx
+++ src/com/asliceofcrazypie/flash/Batcher.hx
@@ -1,7 +1,7 @@
 package com.asliceofcrazypie.flash;
 
-import openfl.geom.Point;
-import openfl.geom.Rectangle;
+import com.asliceofcrazypie.flash.jobs.QuadRenderJob;
+import com.asliceofcrazypie.flash.jobs.TriangleRenderJob;
 import openfl.Vector;
 import flash.display.TriangleCulling;
 import flash.display.BlendMode;
@@ -12,16 +12,46 @@ import flash.display.BlendMode;
  */
 class Batcher
 {
-
-	// TODO: implement it and document it...
-	public static function addTriangles(tilesheet:TilesheetStage3D, vertices:Vector<Float>, indices:Vector<Int> = null, uvtData:Vector<Float> = null, culling:TriangleCulling = null, colors:Vector<Int> = null, smooth:Bool = false, blending:BlendMode = null):Void
+	public static function startQuadBatch(tilesheet:TilesheetStage3D, colored:Bool, blend:BlendMode = null, smooth:Bool = false):QuadRenderJob
 	{
-		
+		return null;
 	}
 	
-	// TODO: implement it and document it...
-	public function addQuad(tilesheet:TilesheetStage3D, tileRect:Rectangle, origin:Point = null, smooth:Bool = false, flags:Int = 0):Void
+	public static function startTrianglesBatch(tilesheet:TilesheetStage3D, colored:Bool = false, blend:BlendMode = null, smoothing:Bool = false):TriangleRenderJob
 	{
-		
+		return null;
 	}
+	/*
+	public static function drawPixels(tilesheet:TilesheetStage3D, sourceRect:Rectangle, origin:Point = null, matrix:Matrix, cr:Float = 1.0, cg:Float = 1.0, cb:Float = 1.0, ca:Float = 1.0, blend:BlendMode = null, smoothing:Bool = false):Void
+	{
+		var colored:Bool = (cr != 1.0) || (cg != 1.0) || (cb != 1.0) || (ca != 1.0);
+		#if !FLX_RENDER_TRIANGLE
+		var drawItem:FlxDrawTilesItem = startQuadBatch(frame.parent, colored, blend, smoothing);
+		#else
+		var drawItem:FlxDrawTrianglesItem = startTrianglesBatch(frame.parent, smoothing, colored, blend);
+		#end
+		drawItem.addQuad(frame, matrix, cr, cg, cb, ca);
+	}
+	
+	public static function copyPixels(tilesheet:TilesheetStage3D, sourceRect:Rectangle, origin:Point = null, destPoint:Point = null, cr:Float = 1.0, cg:Float = 1.0, cb:Float = 1.0, ca:Float = 1.0, blend:BlendMode = null, smoothing:Bool = false):Void
+	{
+		_helperMatrix.identity();
+		_helperMatrix.translate(destPoint.x + frame.offset.x, destPoint.y + frame.offset.y);
+		var colored:Bool = (cr != 1.0) || (cg != 1.0) || (cb != 1.0) || (ca != 1.0);
+		#if !FLX_RENDER_TRIANGLE
+		var drawItem:FlxDrawTilesItem = startQuadBatch(frame.parent, colored, blend, smoothing);
+		#else
+		var drawItem:FlxDrawTrianglesItem = startTrianglesBatch(frame.parent, smoothing, colored, blend);
+		#end
+		drawItem.addQuad(frame, _helperMatrix, cr, cg, cb, ca);
+	}
+	
+	public static function drawTriangles(tilesheet:TilesheetStage3D, vertices:Vector<Float>, indices:Vector<Int>, uv:Vector<Float>, colors:Vector<Int> = null, position:Point = null, blend:BlendMode = null, smoothing:Bool = false):Void
+	{
+		_bounds.set(0, 0, width, height);
+		var isColored:Bool = (colors != null && colors.length != 0);
+		var drawItem:FlxDrawTrianglesItem = startTrianglesBatch(graphic, smoothing, isColored, blend);
+		drawItem.addTriangles(vertices, indices, uvs, colors, position, _bounds);
+	}
+	*/
 }
