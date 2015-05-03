70404dd6adfc2ccddf125a3cf6bc276a77cedca5
Zaphod <zaphy@yandex.ru>
2015-05-03 18:23:31 +0300

    some cleanup again

--- src/com/asliceofcrazypie/flash/jobs/RenderJob.hx
+++ src/com/asliceofcrazypie/flash/jobs/RenderJob.hx
@@ -81,7 +81,116 @@ class RenderJob
 	
 	public function addQuad(rect:Rectangle, origin:Point, uv:Rectangle, matrix:Matrix, r:Float = 1, g:Float = 1, b:Float = 1, a:Float = 1):Void
 	{
+		var imgWidth:Int = Std.int(rect.width);
+		var imgHeight:Int = Std.int(rect.height);
 		
+		var centerX:Float = origin.x * imgWidth;
+		var centerY:Float = origin.y * imgHeight;
+		
+		var px:Float;
+		var py:Float;
+		
+		//top left
+		px = -centerX;
+		py = -centerY;
+		
+		vertices[vertexPos++] = px * matrix.a + py * matrix.c + matrix.tx; //top left x
+		vertices[vertexPos++] = px * matrix.b + py * matrix.d + matrix.ty; //top left y
+		
+		vertices[vertexPos++] = uv.x; //top left u
+		vertices[vertexPos++] = uv.y; //top left v
+		
+		if (isRGB)
+		{
+			vertices[vertexPos++] = r;
+			vertices[vertexPos++] = g;
+			vertices[vertexPos++] = b;
+		}
+		
+		if (isAlpha)
+		{
+			vertices[vertexPos++] = a;
+		}
+		
+		//top right
+		px = imgWidth - centerX;
+		py = -centerY;
+		
+		vertices[vertexPos++] = px * matrix.a + py * matrix.c + matrix.tx; //top right x
+		vertices[vertexPos++] = px * matrix.b + py * matrix.d + matrix.ty; //top right y
+		
+		vertices[vertexPos++] = uv.width; //top right u
+		vertices[vertexPos++] = uv.y; //top right v
+		
+		if (isRGB)
+		{
+			vertices[vertexPos++] = r;
+			vertices[vertexPos++] = g;
+			vertices[vertexPos++] = b;
+		}
+		
+		if (isAlpha)
+		{
+			vertices[vertexPos++] = a;
+		}
+		
+		//bottom right
+		px = imgWidth - centerX;
+		py = imgHeight - centerY;
+		
+		vertices[vertexPos++] = px * matrix.a + py * matrix.c + matrix.tx; //bottom right x
+		vertices[vertexPos++] = px * matrix.b + py * matrix.d + matrix.ty; //bottom right y
+		
+		vertices[vertexPos++] = uv.width; //bottom right u
+		vertices[vertexPos++] = uv.height; //bottom right v
+		
+		if (isRGB)
+		{
+			vertices[vertexPos++] = r;
+			vertices[vertexPos++] = g;
+			vertices[vertexPos++] = b;
+		}
+		
+		if (isAlpha)
+		{
+			vertices[vertexPos++] = a;
+		}
+		
+		//bottom left
+		px = -centerX;
+		py = imgHeight - centerY;
+		
+		vertices[vertexPos++] = px * matrix.a + py * matrix.c + matrix.tx; //bottom left x
+		vertices[vertexPos++] = px * matrix.b + py * matrix.d + matrix.ty; //bottom left y
+		
+		vertices[vertexPos++] = uv.x; //bottom left u
+		vertices[vertexPos++] = uv.height; //bottom left v
+		
+		if (isRGB)
+		{
+			vertices[vertexPos++] = r;
+			vertices[vertexPos++] = g;
+			vertices[vertexPos++] = b;
+		}
+		
+		if (isAlpha)
+		{
+			vertices[vertexPos++] = a;
+		}
+		
+		numVertices += 4;
+		numIndices += 6;
+		
+		/*
+		indices.position = 12 * quadPos; // 12 = 6 * 2 (6 indices per quad and 2 bytes per index)
+		var startIndex:Int = quadPos * 4;
+		indices.writeShort(startIndex + 2);
+		indices.writeShort(startIndex + 1);
+		indices.writeShort(startIndex + 0);
+		indices.writeShort(startIndex + 3);
+		indices.writeShort(startIndex + 2);
+		indices.writeShort(startIndex + 0);
+		*/
 	}
 	
 	public function render(context:ContextWrapper):Void
--- src/com/asliceofcrazypie/flash/jobs/TriangleRenderJob.hx
+++ src/com/asliceofcrazypie/flash/jobs/TriangleRenderJob.hx
@@ -23,102 +23,7 @@ class TriangleRenderJob extends RenderJob
 	{
 		var prevVerticesNumber:Int = Std.int(vertexPos / dataPerVertice);
 		
-		var imgWidth:Int = Std.int(rect.width);
-		var imgHeight:Int = Std.int(rect.height);
-		
-		var centerX:Float = origin.x * imgWidth;
-		var centerY:Float = origin.y * imgHeight;
-		
-		var px:Float;
-		var py:Float;
-		
-		//top left
-		px = -centerX;
-		py = -centerY;
-		
-		vertices[vertexPos++] = px * matrix.a + py * matrix.c + matrix.tx; //top left x
-		vertices[vertexPos++] = px * matrix.b + py * matrix.d + matrix.ty; //top left y
-		
-		vertices[vertexPos++] = uv.x; //top left u
-		vertices[vertexPos++] = uv.y; //top left v
-		
-		if (isRGB)
-		{
-			vertices[vertexPos++] = r;
-			vertices[vertexPos++] = g;
-			vertices[vertexPos++] = b;
-		}
-		
-		if (isAlpha)
-		{
-			vertices[vertexPos++] = a;
-		}
-		
-		//top right
-		px = imgWidth - centerX;
-		py = -centerY;
-		
-		vertices[vertexPos++] = px * matrix.a + py * matrix.c + matrix.tx; //top right x
-		vertices[vertexPos++] = px * matrix.b + py * matrix.d + matrix.ty; //top right y
-		
-		vertices[vertexPos++] = uv.width; //top right u
-		vertices[vertexPos++] = uv.y; //top right v
-		
-		if (isRGB)
-		{
-			vertices[vertexPos++] = r;
-			vertices[vertexPos++] = g;
-			vertices[vertexPos++] = b;
-		}
-		
-		if (isAlpha)
-		{
-			vertices[vertexPos++] = a;
-		}
-		
-		//bottom right
-		px = imgWidth - centerX;
-		py = imgHeight - centerY;
-		
-		vertices[vertexPos++] = px * matrix.a + py * matrix.c + matrix.tx; //bottom right x
-		vertices[vertexPos++] = px * matrix.b + py * matrix.d + matrix.ty; //bottom right y
-		
-		vertices[vertexPos++] = uv.width; //bottom right u
-		vertices[vertexPos++] = uv.height; //bottom right v
-		
-		if (isRGB)
-		{
-			vertices[vertexPos++] = r;
-			vertices[vertexPos++] = g;
-			vertices[vertexPos++] = b;
-		}
-		
-		if (isAlpha)
-		{
-			vertices[vertexPos++] = a;
-		}
-		
-		//bottom left
-		px = -centerX;
-		py = imgHeight - centerY;
-		
-		vertices[vertexPos++] = px * matrix.a + py * matrix.c + matrix.tx; //bottom left x
-		vertices[vertexPos++] = px * matrix.b + py * matrix.d + matrix.ty; //bottom left y
-		
-		vertices[vertexPos++] = uv.x; //bottom left u
-		vertices[vertexPos++] = uv.height; //bottom left v
-		
-		if (isRGB)
-		{
-			vertices[vertexPos++] = r;
-			vertices[vertexPos++] = g;
-			vertices[vertexPos++] = b;
-		}
-		
-		if (isAlpha)
-		{
-			vertices[vertexPos++] = a;
-		}
+		super.addQuad(rect, origin, uv, matrix, r, g, b, a);
 		
 		indicesVector[indexPos++] = prevVerticesNumber + 2;
 		indicesVector[indexPos++] = prevVerticesNumber + 1;
@@ -126,9 +31,6 @@ class TriangleRenderJob extends RenderJob
 		indicesVector[indexPos++] = prevVerticesNumber + 3;
 		indicesVector[indexPos++] = prevVerticesNumber + 2;
 		indicesVector[indexPos++] = prevVerticesNumber + 0;
-		
-		numVertices += 4;
-		numIndices += 6;
 	}
 	
 	public function addTriangles():Void
